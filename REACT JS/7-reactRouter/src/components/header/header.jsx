7db2a1b0168751a6bfa70fd0bccabf58b183de9a
vanug <vanug273@gmail.com>
2024-05-26 22:39:26 +0530

    new

--- REACT JS/7-reactRouter/src/components/header/header.jsx
+++ REACT JS/7-reactRouter/src/components/header/header.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import {Link,NavLink} from 'react-router-dom'
 
-function Header(props) {
-    const {} = props
 
     export default function Header() {
         return (
@@ -37,11 +35,28 @@ function Header(props) {
                             <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                                 <li>
                                     <NavLink
+                                    to="/"
                                         className={({isActive}) =>
-                                            `block py-2 pr-4 pl-3 duration-200 ${isActive? "text-orange-700" : "text-gray-700" } border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                            `block py-2 pr-4 pl-3 duration-200
+                                         ${isActive? "text-orange-700" : "text-gray-700" }
+                                          border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent
+                                           lg:border-0 hover:text-orange-700 lg:p-0`
                                         }
                                     >
                                         Home
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink 
+                                    to="/about"
+                                        className={({isActive}) =>
+                                            `block py-2 pr-4 pl-3 duration-200
+                                         ${isActive? "text-orange-700" : "text-gray-700" }
+                                          border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent
+                                           lg:border-0 hover:text-orange-700 lg:p-0`
+                                        }
+                                    >
+                                        About
                                     </NavLink>
                                 </li>
                                 
@@ -54,8 +69,8 @@ function Header(props) {
         );
     }
     
-    
-}
+ 
+
 
 
 
