e4ef57f535520ffb4a002acdaa9b250f80bf8612
vanug <vanug273@gmail.com>
2024-05-29 00:06:31 +0530

    correct

--- minicontext/src/App.jsx
+++ minicontext/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import UserContextProvider from './Context/UserContextProvider'
+import Login from './components/Login'
+import Profile from './components/Profile'
+import UserContextProvider from './context/UserContextProvider'
 
 function App() {
   
 
   return (
     <UserContextProvider>
-     <h1>React Learning</h1>
+      <h1>React with Chai and share is important</h1>
+      <Login />
+      <Profile />
     </UserContextProvider>
   )
 }
